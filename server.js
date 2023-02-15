221d15272edf9817e86c6ee597179f5df48312c0
AbdulQadir98 <abdul.qadirv98@gmail.com>
2023-02-15 17:28:04 +0530

    user and project check

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 const express = require('express')
 const app = express()
+const { users } = require('./data')
 
-app.get('/', (req, res) => {
+app.use(express.json())
+
+// make setUser middleware execute before every single controller action
+// app.use(setUser)
+
+app.get('/', setUser, (req, res) => {
     res.send('Home Page')
 })
 
-app.get('/admin', (req, res) => {
+app.get('/admin', setUser, (req, res) => {
     res.send('Admin Page')
 })
 
-app.get('/user', (req, res) => {
+app.get('/user',setUser, (req, res) => {
     res.send('User Page')
 })
+
+// require("./routes/projects")(app);
+const projectRouter = require('./routes/projects')
+app.use('/projects', projectRouter)
+
+// middleware to find the user 
+function setUser(req, res, next) {
+    const userId = req.body.userId
+    if (userId) {
+      req.user = users.find(user => user.id === userId)
+    }
+    next()
+}
 
 app.listen(3000)
